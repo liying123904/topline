d21e2199e00dcba0e0a236315626b1e113df9fac
l y <582275558@qq.com>
2020-01-05 17:49:51 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <h2>哈哈哈哈哈 </h2>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
